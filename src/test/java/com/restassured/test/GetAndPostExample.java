f6972a1485b93995a9e5003f84eeaf632c0577ff
Milena Rojas Sepulveda <milena.rojass@perficient.com>
2025-05-29 16:49:26 -0500

    Primera prueba

---
+++ src/test/java/com/restassured/test/GetAndPostExample.java
@@ -0,0 +1,36 @@
+package com.restassured.test;
+
+public class GetAndPostExample {
+}
+
+@Test
+public void testGet() {
+	RestAssured.baseURI = "https://reqres.in/api";
+	
+	when().
+		get("/users?page=2").
+	then().
+		statusCode(200).
+		body("data.size()", is(6)).
+		body("data.first_name", hasItems("George", "Rachel"));
+};
+@Test
+public void testPost() {
+	
+	JSONObject request = new JSONObject();
+	
+	request.put("name", "Ernesto Perez");
+	request.put("job", "QA Automation");
+
+	RestAssured.baseURI =  "https://reqres.in/api";
+	
+	given().
+		header("Content-Type", "application/json").
+		contentType(ContentType.JSON).
+		body(request.toJSONString()).
+	when().
+		post("/users").
+	then().
+		statusCode(201).
+		log().all();
+}
